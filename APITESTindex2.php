7e07ec0b3ee59e4d5d02bf074e61e2e6301e5526
matalords <roger-23-@hotmail.com>
2019-05-26 20:46:57 +0200

    Modified all names of older fiels (Maybe delete?)

---
+++ APITESTindex2.php
@@ -0,0 +1,7 @@
+<html>
+  <a href="APITESTstatus.php">Status</a>
+  <a href="APITESTsummoner.php">Summoner</a>
+  <a href="APITESTchampions.php">Champions</a>
+  <a href="APITESTmatchList.php">MatchList</a>
+  <a href="APITESTmatchDetails.php">MatchDetails</a>
+</html>
--- index2.php
+++
@@ -1,7 +0,0 @@
-<html>
-  <a href="status.php">Status</a>
-  <a href="summoner.php">Summoner</a>
-  <a href="champions.php">Champions</a>
-  <a href="matchList.php">MatchList</a>
-  <a href="matchDetails.php">MatchDetails</a>
-</html>
